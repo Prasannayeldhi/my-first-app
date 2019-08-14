9f4cbec8c76dd458e9ab0d65d79327660d42f247
prasanna <prasanna@prasannas-MacBook-Air.local>
2019-08-14 14:01:14 -0400

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { sanitizeIdentifier } from '@angular/compiler';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  a:number=10;
+  b:number=10;
+  substractionResult:number;
+  multiplicationResult:number;
+  divisionResult:number;
+  students:any;
+  product:any;
+
+  
+  
+
+  constructor(){
+   
+   this.students=[
+     {
+       studentid:13,
+       studentname:'prasanna'
+     },
+     {
+       studentid:12,
+       studentname:'manvitha'
+     },
+     {
+       studentid:15,
+       studentname:'chaithra'
+
+     }
+   ]
+  console.log(this.students)
+  this.product={
+    productid:16,
+    productname:'car'
+  }
+console.log(this.product)
+
+  
+
+
+  }
+  
+}
